b6a1eebce1c6a6e443013f95143bca6c1f1b1d2d
Alkaline <vincent.griebel@yahoo.de>
2015-01-10 18:40:39 +0100

    Add setup

--- Hurricane/Properties/AssemblyInfo.cs
+++ Hurricane/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using Exceptionless.Configuration;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.5.*")]
-[assembly: AssemblyFileVersion("0.2.5")]
+[assembly: AssemblyVersion("0.2.6.*")]
+[assembly: AssemblyFileVersion("0.2.6")]
